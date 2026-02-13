30c238afee7c4d3a534b6fc0c47076ac8276a85c
Garson Click <alltheclicks@pm.me>
2026-02-13 17:33:14 +0100

    feat(api): LP-0303 add series episode stream URL builder

--- packages/api/src/xtream-codes-service.ts
+++ packages/api/src/xtream-codes-service.ts
@@ -136,6 +136,16 @@ export class XtreamCodesService {
     return `${this.credentials.server}/movie/${this.credentials.username}/${this.credentials.password}/${streamId}.${extension}`;
   }
 
+  getSeriesEpisodeStreamUrl(
+    episodeId: number,
+    extension = "mp4",
+  ): string {
+    if (!this.credentials) {
+      throw new Error("Credentials not set");
+    }
+    return `${this.credentials.server}/series/${this.credentials.username}/${this.credentials.password}/${episodeId}.${extension}`;
+  }
+
   getCatchUpUrl(
     streamId: number,
     startTime: number,
